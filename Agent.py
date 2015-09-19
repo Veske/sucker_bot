40296bfc2e01b04734773f8362c78613f53d8069
Mr Leo <mrleo@seductionarts.info>
2015-09-19 19:24:43 +0300

    Added room cleaning

--- Agent.py
+++ Agent.py
@@ -8,14 +8,17 @@ class Agent:
     def __init__(self, rooms):
         self.rooms = rooms
         self.room = rooms[randint(0, 1)]
+        self.score = 0;
 
     def suck(self, room):
-        room.cleanRoom()
+        print("State: Sucking Room:[",self.room.getName(),",",self.room.getState(),"]")
+        self.room.cleanRoom()
 
     def no_op(self):
         return
 
     def left(self):
+        self.score-=1;
         if(self.room.getName() != "A"):
             self.room = self.rooms[0]
             print("Cleaning: ", self.room.getName(), "State after work: ", self.room.getState())
@@ -24,6 +27,7 @@ class Agent:
             return
 
     def right(self):
+        self.score-=1;
         if(self.room.getName() != "B"):
             self.room = self.rooms[1]
             print("Cleaning: ", self.room.getName(), "State after work: ", self.room.getState())
@@ -33,12 +37,17 @@ class Agent:
 
 
     def run(self, steps = 10):
+
         for i in range(steps):
+            self.score+=1;
             if(self.room.getState() == "DIRTY"):
-                self.room.cleanRoom()
+                self.suck(self)
                 self.choose_room(randint(0, 1) )
             else:
                 self.choose_room(randint(0, 1) )
+                self.score +=1
+
+        print("Efektiivsus Skoor: ", self.score)
 
     def choose_room(self, choice):
         if(choice == 0):
