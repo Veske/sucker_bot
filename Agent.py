fe50ea23db24c3e304d0b254c4021af74f4a592f
Veske <veske@tlu.ee>
2015-09-22 21:28:22 +0300

    Score for agent 1

--- Agent.py
+++ Agent.py
@@ -1,6 +1,7 @@
 __author__ = 'kaspar'
 
 from random import randint
+from Colors import bcolors
 
 class Agent:
     room = ""
@@ -8,37 +9,44 @@ class Agent:
     def __init__(self, rooms):
         self.rooms = rooms
         self.room = rooms[randint(0, 1)]
+        self.score = 1
 
     def suck(self, room):
+        self.score -= 1
+        print(bcolors.OKGREEN + "Cleaning room: ", self.room.getName())
         room.cleanRoom()
 
     def no_op(self):
         return
 
     def left(self):
+        self.score -= 1
         if(self.room.getName() != "A"):
             self.room = self.rooms[0]
-            print("Cleaning: ", self.room.getName(), "State after work: ", self.room.getState())
+            print("Moving to room: ", self.room.getName(), "Room state: ", self.room.getState())
         else:
-            print("Cleaning: ", self.room.getName(), "State after work: ", self.room.getState())
-            return
+            self.no_op()
 
     def right(self):
+        self.score -= 1
         if(self.room.getName() != "B"):
             self.room = self.rooms[1]
-            print("Cleaning: ", self.room.getName(), "State after work: ", self.room.getState())
+            print("Moving to room: ", self.room.getName(), "Room state: ", self.room.getState())
         else:
-            print("Cleaning: ", self.room.getName(), "State after work: ", self.room.getState())
-            return
+            self.no_op()
 
 
     def run(self, steps = 10):
         for i in range(steps):
+            self.score += 1
+            print(bcolors.BOLD + "Step: ", i)
+            print(bcolors.FAIL + "Current room states:\nROOM A: ", self.rooms[0].getState(), "\tROOM B: ", self.rooms[1].getState() + bcolors.ENDC)
             if(self.room.getState() == "DIRTY"):
-                self.room.cleanRoom()
-                self.choose_room(randint(0, 1) )
+                self.suck(self.room)
             else:
                 self.choose_room(randint(0, 1) )
+
+        print(bcolors.OKBLUE + "\nScore after operation: ", self.score, bcolors.ENDC)
 
     def choose_room(self, choice):
         if(choice == 0):
--- Simulator.py
+++ Simulator.py
@@ -1,6 +1,5 @@
 __author__ = 'kaspar'
 
-import msvcrt
 import Room
 from Agent import Agent
 from Agent2 import Agent2
@@ -16,24 +15,9 @@ class Simulator:
     def get_room2(self):
         return self.rooms[1]
 
-
 c = Simulator()
 
 #a = Agent(c.rooms)
-a = Agent2(c.rooms)
+a = Agent(c.rooms)
 
 a.run()
-
-
-
-
-
-
-
-
-
-#print(a.left())
-#print(c.get_room1().getName())
-#a.suck(c.get_room2())
-#print(c.get_room1().getName(), c.get_room1().getState())
-#print(c.get_room2().getName(), c.get_room2().getState())
